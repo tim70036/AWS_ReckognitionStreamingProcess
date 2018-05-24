fa9c134bd3dde1735e6fa5aa0ca90b704f2c50fd
tim70036 <tim70036@gmail.com>
2018-05-24 21:33:26 +0800

    init

---
+++ KinesisConsumer.py
@@ -0,0 +1,136 @@
+###############################################################
+###############################################################
+##		This program pulls data from Kinesis Data Stream.	 ##
+##		You should modify Extract() to customize the outputs ##
+##		that are extracted from data stream.				 ##
+###############################################################
+###############################################################
+
+import boto3
+import json
+from datetime import datetime
+import time
+import pprint
+
+# import var set up
+from var import *
+# Set your own variables in a file named var.py: 
+		# AWS_REGION
+		# streamName
+		# collectionId
+		# videoStreamArn
+		# dataStreamArn
+		# streamProcessorName
+		# IAMRoleArn -> rekognition role for processor
+		
+client = boto3.client('kinesis', region_name=AWS_REGION)
+
+##########################################################
+##########################################################
+def main():
+
+	CheckStream(streamName)
+
+	shardIter = GetShardIter(streamName)
+
+	ConsumeStream(shardIter)
+##########################################################
+##########################################################
+def ExtractData(response):
+	# Customize processing data
+	recordArr = response["Records"]
+
+	# Empty?
+	if not recordArr:
+		print('This is an Empty Record.')
+		return
+
+	# Iterate each record
+	for obj in recordArr:
+
+		# Data record time
+		time = obj["ApproximateArrivalTimestamp"]
+		print("Record Time :" + time.strftime("%Y-%m-%d %H:%M:%S"))
+
+		# Decode binary data to JSON string , and then to dict
+		data = obj["Data"].decode()
+		data = json.loads(data)
+
+		# Output certrain field
+		print("FaceSearchResponse :")
+		if "FaceSearchResponse" in data:
+			pprint.pprint(data["FaceSearchResponse"], indent=1)
+		else:
+			pprint.pprint("FaceSearchResponse does not exist in this record", indent=1)
+
+##########################################################
+##########################################################
+def ConsumeStream(shardIter):
+	print('=====================================')
+	print('Start getting record...')
+
+	# Infinite Loop
+	while True:
+
+		response = client.get_records(ShardIterator=shardIter, Limit=1)
+		ExtractData(response)
+		print('=====================================')
+		time.sleep(3)
+
+		# If there is new data 
+		while 'NextShardIterator' in response:
+			response = client.get_records(ShardIterator=response['NextShardIterator'], Limit=1)
+			ExtractData(response)
+			print('=====================================')
+			# wait for 3 seconds
+			time.sleep(3)
+##########################################################
+##########################################################
+def CheckStream(streamName):
+	print('Listing all stream : ')
+	response = client.list_streams()
+	isCreated = False
+	for stream in response['StreamNames']:
+		print(stream)
+		if streamName == stream :
+			isCreated = True
+	print('=====================================')
+
+	# Create stream if not exist
+	if not isCreated:
+		print('Creating ' + streamName + ' ...')
+		client.create_stream(StreamName = streamName, ShardCount=2)
+		while True:
+			response = client.describe_stream()
+
+			if response['StreamDescription']['StreamStatus'] == 'ACTIVE' :
+				break
+			print(streamName + ' is not active yet...')
+			time.sleep(3)
+	else :
+		print('Exist ' + streamName + ', no need for creating.')
+	print('=====================================')
+##########################################################
+##########################################################
+def GetShardIter(streamName):
+	print('Getting ShardIter')
+	# Get stream shard ID
+	response = client.describe_stream(StreamName=streamName)
+	shardID = response['StreamDescription']['Shards'][0]['ShardId']
+	print('Receive shard id : ' + shardID)
+
+
+	shardIter = client.get_shard_iterator(
+	    StreamName=streamName,
+	    ShardId=shardID,
+	    ShardIteratorType='LATEST',
+	)
+	shardIter = shardIter['ShardIterator']
+
+	print('Receive shard iterator.')
+	return shardIter
+##########################################################
+##########################################################
+if __name__ == '__main__':
+	main()
+
---
+++ StreamProcessor.py
@@ -0,0 +1,141 @@
+###############################################################
+###############################################################
+##		This program set up a Rekognition Stream Processor.	 ##
+##		This Processor pulls streaming data from Kinesis 	 ##
+##		Video Stream. After streaming data is processed, 	 ##
+##		the result is sent to Kinesis Data Stream.			 ##
+###############################################################
+###############################################################
+
+import boto3
+import time
+import datetime
+import sys
+
+# Import var set up
+from var import *
+# Set your own variables in a file named var.py: 
+		# AWS_REGION
+		# streamName
+		# collectionId
+		# videoStreamArn
+		# dataStreamArn
+		# streamProcessorName
+		# IAMRoleArn -> rekognition role for processor
+
+client = boto3.client('rekognition', region_name=AWS_REGION)
+
+##########################################################
+##########################################################
+def main():
+
+	# Create collection if not exist
+	CheckCollection()
+
+	# Create processor if not exist
+	CheckProcessor()
+
+	# Start processor if not running
+	StartProcessor()
+##########################################################
+##########################################################
+def CheckCollection():
+
+	print('Checking collections.')
+	response = client.list_collections()
+
+	# Create empty collection if not exist
+	if collectionId not in response['CollectionIds']:
+		collection = client.create_collection(CollectionId=collectionId)
+		print(collectionId + ' is created.')
+	else:
+		print(collectionId + ' exists.')
+##########################################################
+##########################################################
+def CheckProcessor():
+
+	print('\nListing all processor : ')
+	response = client.list_stream_processors()
+	isCreated = False
+	for processor in response['StreamProcessors']:
+		print(processor['Name'] +'->' + processor['Status'])
+		if streamProcessorName == processor['Name']:
+			isCreated = True
+
+	print('')
+	if not isCreated:
+		# Create stream processor
+		print('Creating streamProcessor...')
+		streamProcessorArn = client.create_stream_processor(
+		    Input={
+		        'KinesisVideoStream': {
+		            'Arn': videoStreamArn
+		        }
+		    },
+		    Output={
+		        'KinesisDataStream': {
+		            'Arn': dataStreamArn
+		        }
+		    },
+		    Name=streamProcessorName,
+		    Settings={
+		        'FaceSearch': {
+		            'CollectionId': collectionId,
+		            'FaceMatchThreshold': 90
+		        }
+		    },
+		    RoleArn=IAMRoleArn
+		)
+		print('Created streamProcessorArn : ' + streamProcessorArn['StreamProcessorArn'])
+	else:
+		print('Exist ' + streamProcessorName + ', no need for creating.')
+##########################################################
+##########################################################
+def StopProcessor():
+
+	response = client.describe_stream_processor(Name=streamProcessorName)
+	if response['Status'] != "Stopped":
+		print('Current state of processor->' + response['Status'])
+		response = client.stop_stream_processor(Name=streamProcessorName)
+		print('Stopping ' + streamProcessorName + '...')
+	else:
+		print(streamProcessorName + 'is already stopped.')
+
+	# Monitor until it is stopped 
+	response = client.describe_stream_processor(Name=streamProcessorName)
+	while response['Status'] != "STOPPED":
+		print('Current state of processor->' + response['Status'])
+		response = client.describe_stream_processor(Name=streamProcessorName)
+		time.sleep(5)
+##########################################################
+##########################################################
+def StartProcessor():
+
+	# Start stream processor
+	response = client.describe_stream_processor(Name=streamProcessorName)
+	if response['Status'] != "RUNNING":
+		print('Current state of processor->' + response['Status'])
+		response = client.start_stream_processor(Name=streamProcessorName)
+		print('Starting ' + streamProcessorName + '...')
+	else:
+		print(streamProcessorName + 'is already running.')
+
+	# Infinite loop with keyboard interrupt detection
+	try:
+		while True:
+			response = client.describe_stream_processor(Name=streamProcessorName)
+			print('		==================================')
+			print('		LastUpdateTime : ' + response['LastUpdateTimestamp'].strftime("%Y-%m-%d %H:%M:%S"))
+			print('		Status : ' + response['Status'])
+			print('		Input  : ' + response['Input']['KinesisVideoStream']['Arn'])
+			print('		Output : ' + response['Output']['KinesisDataStream']['Arn'])
+			print('		Press ctrl+c to leave')
+			time.sleep(3)
+	except KeyboardInterrupt:
+		print('\nKeyboardInterrupt detected!')
+		StopProcessor()
+		sys.exit()
+##########################################################
+##########################################################
+if __name__ == '__main__':
+	main()
